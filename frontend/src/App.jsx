35e4f22e0a2711f59313132408ec520a275691a8
Piyush Bihani <piyushbihani6575@gmail.com>
2025-08-28 23:36:23 +0530

    Update project branding and enhance theme management
    - Changed the title in index.html from "Vite + React" to "ChatKaro".
    - Updated package dependencies, downgrading daisyui from version 5.0.54 to 4.12.14.
    - Enhanced tailwind.config.js to include a comprehensive list of themes for daisyui.
    - Introduced a new useThemeStore for managing theme state with local storage persistence.
    - Developed a SettingsPage component to allow users to select and preview themes.
    - Refactored App component to apply the selected theme dynamically.
    - Added a constants file to store theme options for better maintainability.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,12 +8,14 @@ import ProfilePage from './pages/ProfilePage';
 import Navbar from './components/Navbar';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAuthStore } from '../store/useAuthStore.js';
+import { useThemeStore } from '../store/useThemeStore.js';
 import { useEffect } from 'react';
 import { Loader } from 'lucide-react';
 import {Toaster} from "react-hot-toast"
 
 const App = () => {
   const {authUser,checkAuth,isCheckingAuth}=useAuthStore();
+ const {theme}=useThemeStore();
   useEffect(()=>{
     checkAuth();
   },[checkAuth])
@@ -26,13 +28,13 @@ const App = () => {
  )
 
   return (
-    <div>
+    <div data-theme={theme}> 
       <Navbar/>
       <Routes>
         <Route path='/' element={authUser?<HomePage/>:<Navigate to="/login"/>}/>
         <Route path='/signup' element={!authUser?<SignUpPage/> :<Navigate to="/"/>}/>
         <Route path='/login' element={!authUser?<LoginPage/> :<Navigate to="/"/>}/>
-        <Route path='/setting' element={<SettingsPage/>}/>
+        <Route path='/settings' element={<SettingsPage/>}/>
         <Route path='/profile' element={authUser?<ProfilePage/>:<Navigate to="/login"/>}/>
       </Routes>
       <Toaster/>
